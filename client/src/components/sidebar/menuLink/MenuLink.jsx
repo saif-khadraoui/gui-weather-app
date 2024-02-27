f8d9eff7642c9286cbcbf5ac5fff184240bd10d7
saifk <ec22616@qmul.ac.uk>
2024-02-27 19:17:23 +0000

    test

---
+++ client/src/components/sidebar/menuLink/MenuLink.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import styles from "./menuLink.module.css"
+import { Link, useLocation } from 'react-router-dom'
+
+function MenuLink({ item }) {
+    const pathname = useLocation()
+    console.log(pathname.pathname)
+  return (
+    <Link to={item.path} className={`${styles.container} ${pathname.pathname === item.path && styles.active}`}>
+        {item.icon}
+        {item.title}
+        test
+    </Link>
+  )
+}
+
+export default MenuLink
